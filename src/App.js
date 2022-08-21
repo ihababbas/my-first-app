d622e00fde1d029436df4e2d4952f903612be7f4
ihababbas <ihababbas26@gmail.com>
2022-08-21 23:26:02 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+//import HornedBeast from './HornedBeast';
+class App extends React.Component{
+render(){
+return (
+<div>
+<header>
+  <h1>first class 301!</h1>
+</header>
+
+<main>
+
+</main>
+
+<footer>
+  <h3>ihab abbas</h3>
+</footer>
+
+
+
+
+</div>
+
+)
+}
+
 }
 
 export default App;
---
+++ src/HornedBeast.js
@@ -0,0 +1,10 @@
+/*import React from 'react';
+
+class HornedBeast extends React.Component
+{
+ render(){
+ // return();
+ }
+}
+
+export default HornedBeast ;
