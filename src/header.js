54007be8a4db181c14d6a9ab7406279b81dd405f
ihababbas <ihababbas26@gmail.com>
2022-08-22 11:17:56 +0300

    done

---
+++ src/header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class header extends React.Component
+{
+ render(){
+
+    return(
+      <div>
+     
+     <h1>my first app</h1>
+
+      </div>
+    )
+ }
+}
+
+export default header ;
